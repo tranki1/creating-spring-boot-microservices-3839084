6655fb6e5746551c1f2a6f55e28ca55fe31469aa
tranki1 <trankimhieu1991@gmail.com>
2024-11-10 16:47:02 +0200

    chore: complete challenge

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
@@ -1,8 +1,15 @@
 package com.example.explorecalijpa.repo;
 
+import com.example.explorecalijpa.model.Difficulty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.explorecalijpa.model.Tour;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TourRepository extends JpaRepository<Tour, Integer> {
+    List<Tour> findByDifficulty(Difficulty difficulty);
+
+    List<Tour> findByTourPackageCode(String tourPackageCode);
 }
