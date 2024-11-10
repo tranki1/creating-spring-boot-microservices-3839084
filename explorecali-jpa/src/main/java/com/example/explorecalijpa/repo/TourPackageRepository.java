41368834cf6ed51dd0fa53c36e94fb7b977f82d0
tranki1 <trankimhieu1991@gmail.com>
2024-11-10 02:22:54 +0200

    chore: jpa declareation

---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourPackageRepository.java
@@ -0,0 +1,9 @@
+package com.example.explorecalijpa.repo;
+
+import com.example.explorecalijpa.model.TourPackage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface TourPackageRepository extends JpaRepository<TourPackage, String> {
+
+}
---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
@@ -0,0 +1,8 @@
+package com.example.explorecalijpa.repo;
+
+import com.example.explorecalijpa.model.Tour;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TourRepository extends JpaRepository<Tour, Integer> {
+
+};
